dc6451b6ab6893b33278221ab63f5e65ce36cfbf
emmyxx <emmy.alquie@etu.umontpellier.fr>
2023-04-17 15:06:46 +0200

    refactor: utilise la fonction close pour fermer le socket
    On a mis la fonction close à la place du shutdown
     pour suivre la syntaxe du cours.

--- serveur.c
+++ serveur.c
@@ -5,8 +5,9 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
+
   printf("Début programme\n");
 
   // Création de la socket
@@ -14,6 +15,12 @@ int main(int argc, char *argv[]) {
   // SOCK_STREAM : protocole TCP
   int dS = socket(PF_INET, SOCK_STREAM, 0);
   printf("Socket Créé\n");
+
+  if (dS == -1)
+  {
+    perror("Erreur de création de la socket");
+    exit(EXIT_FAILURE);
+  }
 
   // Configuration de l'adresse
   // AF_INET : IPv4
@@ -23,33 +30,58 @@ int main(int argc, char *argv[]) {
   ad.sin_addr.s_addr = INADDR_ANY;
   // hton convertir un entier en format réseau
   ad.sin_port = htons(atoi(argv[1]));
-  
-  // Associer l'adresse à la socket
-  bind(dS, (struct sockaddr*)&ad, sizeof(ad));
-  printf("Socket Nommé\n");
 
+  // Associer l'adresse à la socket
+  if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) == -1)
+  {
+    perror("Erreur de nommage de la socket");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Socket Nommé\n");
   // Passer la socket en mode écoute
-  listen(dS, 7);
+  if (listen(dS, 7) == -1)
+  {
+    perror("Erreur de passage en mode écoute");
+    exit(EXIT_FAILURE);
+  }
+
   printf("Mode écoute\n");
 
   // Accepter la connexion entrante
   struct sockaddr_in aC;
   socklen_t lg = sizeof(struct sockaddr_in);
-  int dSC = accept(dS, (struct sockaddr*) &aC, &lg);
+  int dSC = accept(dS, (struct sockaddr *)&aC, &lg);
   printf("Client Connecté\n");
+
+  if (dSC == -1)
+  {
+    perror("Erreur de connexion");
+    exit(EXIT_FAILURE);
+  }
 
   // Recevoir le message du client
   char msg[20];
-  recv(dSC, msg, sizeof(msg), 0);
+  if (recv(dSC, msg, sizeof(msg), 0) == -1)
+  {
+    perror("Erreur de reception ");
+    exit(EXIT_FAILURE);
+  }
+
   printf("Message reçu : %s\n", msg);
-  
+
   // Préparer le message à envoyer au client
   int r = 10;
- 
+
   // Envoyer le message au client
-  send(dSC, &r, sizeof(int), 0);
+  if (send(dSC, &r, sizeof(int), 0) == -1)
+  {
+    perror("Erreur d'envoie");
+    exit(EXIT_FAILURE);
+  }
+
   printf("Message Envoyé\n");
-  
+
   // Fermer la connexion avec le client et fermer la socket
   close(dSC);
   close(dS);
