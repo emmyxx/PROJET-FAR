8334b56857f643fe37df3296fa5e3080c1383ba1
louis-marceron <louis.marceron@protonmail.com>
2023-04-25 09:13:09 +0200

    refactor: met les include dans le header et pas dans le .c

--- include/common.h
+++ include/common.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #include "../src/common.c"
 
 #define TAILLE_MESSAGE 256
--- src/common.c
+++ src/common.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-
-#define TAILLE_MESSAGE 256
-
 void gestionnaireErreur(const char *message)
 {
   perror(message);
