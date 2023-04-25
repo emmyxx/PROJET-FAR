999221fccc5bb2710f98f3d3796a7ee3cd204656
louis-marceron <louis.marceron@protonmail.com>
2023-04-25 09:11:36 +0200

    refactor: met les includes dans le header common

--- src/common.c
+++ src/common.c
@@ -1,5 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define TAILLE_MESSAGE 256
 
